d7eff7cd43df48a5802ceb789c987320ea0670dc
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-07-12 13:16:26 +0530

    new

--- src/components/AverageBlock.jsx
+++ src/components/AverageBlock.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 function AverageBlock() {
   return (
-    <div>AverageBlock</div>
+    <div className='bg-[#b8b7b7] h-full p-[2rem]  rounded-xl'>
+      <div className='flex justify-between items-center'>
+        <div>
+          <div className='h-full w-[2px]'></div>
+          <p>Average block time</p>
+          <h2>10 seconds</h2>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
--- src/components/CurrentEpoch.jsx
+++ src/components/CurrentEpoch.jsx
@@ -3,7 +3,34 @@ import React from 'react'
 function CurrentEpoch() {
   return (
     <div className='bg-[#d5f7ee] h-full p-[2rem] mt-3 rounded-xl'>
-      CurrentEpoch</div>
+      
+    <div className='mb-3'>
+      <h1 className=' text-base sm:text-lg font-[600] text-green-600'>Current Epoch</h1>
+      <p className=' text-base sm:text-lg font-[600] text-green-600'>90</p>
+
+      
+      </div> 
+      <div>
+      <p className='font=[500] text-base text-slate-600'>9h 56m left</p>
+      <div className='relative w-full rounded-full bg-white'>
+        <div className='py-1 bg-green-500 rounded-full w-[58%]'>
+
+        </div>
+      </div>
+      <div className='mb-2'>
+        <h2>Started</h2>
+        <p>10:35 PM, Yesterday</p>
+      </div>
+      <div>
+        <div>
+          <p>Checkpoint</p>
+        </div>
+        <div>
+          <p>7,456,636</p>
+        </div>
+      </div>
+      </div> 
+    </div>
   )
 }
 
