de4651f5e896d100b9b2d1ee6d94f6673f28b593
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-07-12 15:33:25 +0530

    new

--- src/sections/BlockSection.jsx
+++ src/sections/BlockSection.jsx
@@ -2,7 +2,39 @@ import React from 'react'
 
 function BlockSection() {
   return (
-    <div>BlockSection</div>
+    <section className="mt-12">
+    <div className="max-w-6xl mx-auto">
+      <div className='card bg-white h-full rounded-xl border border-gray-45  p-[3.1rem]   mb-2'>
+
+<div className='flex justify-between items-center'>
+  <h1>Blocks</h1>
+  <button>View All Blocks</button>
+
+</div>
+<div className='flex justify-between items-center'>
+  <div>
+    block 1
+  </div>
+
+  <div>
+    block 1
+  </div>
+
+  <div>
+    block 1
+  </div>
+
+  <div>
+    block 1
+  </div>
+  <div>
+    block 1
+  </div>
+
+</div>
+      </div>
+      </div>
+      </section>
   )
 }
 
--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -2,7 +2,106 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <>
+    <footer id="masterFooter" class="bg-light mt-auto d-print-none">
+<div class="container-xxl">
+<div class="d-flex justify-content-between align-items-baseline py-6">
+
+<div class="d-flex gap-2">
+<a class="btn btn-sm btn-secondary content-center rounded-circle" style={{width:'2rem',height:'2rem'}} href="https://twitter.com/etherscan" rel="nofollow noopener" target="_blank" data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-placement="top" aria-label="Twitter">
+<i class="fab fa-twitter"></i>
+</a>
+<a class="btn btn-sm btn-secondary content-center rounded-circle" style={{width:'2rem',height:'2rem'}} href="https://medium.com/etherscan-blog" rel="nofollow noopener" target="_blank" data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-placement="top" aria-label="Medium">
+<i class="fab fa-medium"></i>
+</a>
+<a class="btn btn-sm btn-secondary content-center rounded-circle" style={{width:'2rem',height:'2rem'}} href="https://www.facebook.com/etherscan/" rel="nofollow noopener" target="_blank" data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-placement="top" aria-label="Facebook">
+<i class="fab fa-facebook"></i>
+</a>
+<a class="btn btn-sm btn-secondary content-center rounded-circle" style={{width:'2rem',height:'2rem'}} href="https://www.reddit.com/r/etherscan/" rel="nofollow noopener" target="_blank" data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-placement="top" aria-label="Reddit">
+<i class="fab fa-reddit-alien"></i>
+</a>
+</div>
+
+<a class="link-dark" href="/">
+<i class="far fa-arrow-up-to-line me-1"></i>Back to Top
+</a>
+</div>
+<hr class="my-0"/>
+
+<div class="row justify-content-md-between py-8 py-lg-10">
+<div class="col-lg-4 pe-xl-16 mb-4 mb-lg-0">
+<div class="d-flex align-items-center mb-3">
+<img class="me-2" width="20" data-img-theme="light" src="/images/svg/brands/ethereum-original.svg" alt="Ethereum Logo"/>
+<img class="me-2" width="20" data-img-theme="darkmode" src="/images/svg/brands/ethereum-original-light.svg" alt="Ethereum Logo"/>
+<span class="fs-5">Powered by Ethereum</span>
+</div>
+<p class="fs-sm">Etherscan is a Block Explorer and Analytics Platform for Ethereum, a decentralized smart contracts platform.</p>
+<div class="d-none d-lg-block mt-n4 mb-n6">
+<img class="opacity-50" width="280" data-img-theme="light" src="/images/map.png" alt="Background Map Image"/>
+<img class="opacity-50" width="280" data-img-theme="darkmode" src="/images/map-light.png" alt="Background Map Image"/>
+</div>
+</div>
+<div class="col-6 col-md-4 col-lg mb-10 mb-md-0">
+<h4 class="h6 mb-3">Company</h4>
+
+<ul class="list-unstyled list-sm-space fs-sm mb-0">
+<li><a class="link-dark" href="/aboutus">About Us</a></li>
+<li><a class="link-dark" href="/brandassets">Brand Assets</a></li>
+<li><a class="link-dark" href="/contactus">Contact Us</a></li>
+<li>
+<a class="link-dark" href="/careers">
+<span class="me-1">Careers</span> <span class="bg-primary text-white small fw-medium text-nowrap rounded-pill p-1 px-2">We're Hiring!</span>
+</a>
+</li>
+<li><a class="link-dark" href="/terms">Terms of Service</a></li>
+<li><a class="link-dark" href="/bugbounty">Bug Bounty</a></li>
+</ul>
+
+</div>
+<div class="col-6 col-md-4 col-lg mb-10 mb-md-0">
+<h4 class="h6 mb-3">Community</h4>
+
+<ul class="list-unstyled list-sm-space fs-sm mb-0">
+<li><a class="link-dark" href="https://docs.etherscan.io/" target="_blank">API Documentation</a></li><a class="link-dark" href="https://docs.etherscan.io/" target="_blank">
+</a><li><a class="link-dark" href="https://docs.etherscan.io/" target="_blank"></a><a class="link-dark" href="https://info.etherscan.com/">Knowledge Base</a></li>
+<li><a class="link-dark" href="https://etherscan.freshstatus.io/" rel="nofollow noopener" target="_blank">Network Status</a></li>
+<li><a class="link-dark" href="https://info.etherscan.com/etherscan-newsletters/">Newsletters</a></li>
+<li><a class="link-dark" href="/comments">Disqus Comments</a></li>
+</ul>
+
+</div>
+<div class="col-6 col-md-4 col-lg">
+<h4 class="h6 mb-3">Products &amp; Services</h4>
+
+<ul class="list-unstyled list-sm-space fs-sm mb-0">
+<li><a class="link-dark" href="/contactusadvertise">Advertise</a></li>
+<li><a class="link-dark" href="/eaas">Explorer-as-a-Service (EaaS)</a></li>
+<li><a class="link-dark" href="/apis">API Plans</a></li>
+<li><a class="link-dark" href="/prioritysupport">Priority Support</a></li>
+<li><a class="link-dark" href="https://blockscan.com" target="_blank">Blockscan <i class="far fa-arrow-up-right-from-square text-muted ms-1"></i></a></li>
+<li><a class="link-dark" href="https://chat.blockscan.com/start" target="_blank">Blockscan Chat <i class="far fa-arrow-up-right-from-square text-muted ms-1"></i></a></li>
+</ul>
+
+</div>
+</div>
+
+
+<div class="border-top py-4">
+<div class="row justify-content-between align-items-center fs-sm">
+<div class="col-md mb-2 mb-md-0">
+<p class="mb-0">Etherscan © 2023 (F1)</p>
+</div>
+<div class="col-md text-md-end">
+<p class="mb-0">
+Donations: <a class="me-1" href="/address/0x71c7656ec7ab88b098defb751b7401b5f6d8976f"><span id="spanDonateAddress" runat="server">0x71c765...d8976f</span></a> <i class="fas fa-heart text-danger"></i>
+</p>
+</div>
+</div>
+</div>
+
+</div>
+</footer>
+    </>
   )
 }
 
