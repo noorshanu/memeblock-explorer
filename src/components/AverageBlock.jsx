bced2b4e2baf18dc8bd5dc521b4b7f39ad1f673b
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-07-12 15:03:24 +0530

    Update AverageBlock.jsx

--- src/components/AverageBlock.jsx
+++ src/components/AverageBlock.jsx
@@ -5,9 +5,9 @@ function AverageBlock() {
     <div className="bg-white h-full rounded-xl border border-gray-45  p-[3.1rem]   mb-2">
       <div className="flex justify-between items-center">
         <div className="flex">
-          <div className=" w-[2px] bg-white rounded-xl"></div>
+          <div className=" w-[4px] bg-black rounded-full"></div>
           <div className="h-full">
-            <p>Average block time</p>
+            <p className="">Average block time</p>
             <h2>10 seconds</h2>
           </div>
         </div>
