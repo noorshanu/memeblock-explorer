75423376f74c4cf8968e6974d506162c4995bb66
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-07-13 14:12:28 +0530

    font

--- src/components/AverageBlock.jsx
+++ src/components/AverageBlock.jsx
@@ -4,11 +4,11 @@ function AverageBlock() {
   return (
     <div className="card-tran bg-white h-full rounded-xl border border-gray-45  p-[3.1rem]   mb-2">
       <div className="flex justify-between items-center">
-        <div className="flex">
+        <div className="flex gap-2 ">
           <div className=" w-[4px] bg-black rounded-full"></div>
           <div className="h-full">
-            <p className="">Average block time</p>
-            <h2>10 seconds</h2>
+            <p className="text-[#248eff] text-sm">Average block time</p>
+            <h2 className=" text-xl">10 seconds</h2>
           </div>
         </div>
 
