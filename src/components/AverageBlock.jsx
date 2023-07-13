c8f38a7c7fb1207b2835c47c76a3fa8163f67e78
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-07-14 00:04:13 +0530

    Update AverageBlock.jsx

--- src/components/AverageBlock.jsx
+++ src/components/AverageBlock.jsx
@@ -5,7 +5,7 @@ function AverageBlock() {
     <div className="card-tran bg-white h-full rounded-xl border border-gray-45  p-[3.5rem]   mb-2">
       <div className="flex flex-col sm:flex-row justify-between text-start gap-2 sm:gap-0 sm:items-center">
         <div className="flex gap-2 ">
-          <div className=" w-[4px] bg-black rounded-full"></div>
+          <div className=" w-[4px] bg-[#c2c0c0] rounded-full"></div>
           <div className="h-full">
             <p className="text-[#248eff] text-sm">Average block time</p>
             <h2 className=" text-xl">10 seconds</h2>
@@ -13,7 +13,7 @@ function AverageBlock() {
         </div>
 
         <div className="flex gap-2">
-          <div className=" w-[4px] bg-black rounded-full"></div>
+          <div className=" w-[4px] bg-[#c2c0c0] rounded-full"></div>
           <div className="h-full">
             <p className="text-[#248eff] text-sm">Total transactions</p>
             <h2 className="text-xl">1</h2>
@@ -21,7 +21,7 @@ function AverageBlock() {
         </div>
 
         <div className="flex gap-2">
-          <div className=" w-[4px]  bg-black rounded-full"></div>
+          <div className=" w-[4px]  bg-[#c2c0c0] rounded-full"></div>
           <div className="h-full">
             <p className="text-[#248eff] text-sm">Total blocks</p>
             <h2 className="text-xl">16,460</h2>
@@ -29,7 +29,7 @@ function AverageBlock() {
         </div>
 
         <div className="flex gap-2">
-          <div className=" w-[4px]  bg-black rounded-full"></div>
+          <div className=" w-[4px]  bg-[#c2c0c0] rounded-full"></div>
           <div className="h-full">
             <p className="text-[#248eff] text-sm">Wallet addresses</p>
             <h2 className="text-xl">4</h2>
