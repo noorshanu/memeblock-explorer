383f8ce8c32e3b2a28ab52b2521d5e7cb98e8b7a
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-07-18 04:16:23 +0530

    Update Dropdown2.jsx

--- src/components/Dropdown2.jsx
+++ src/components/Dropdown2.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-const DropdownButton2 = ({Title,item1,item2,item3,link1,link2,link3}) => {
+const DropdownButton2 = ({Title,item1,item2,item3,link1,link2,link3,Trans,item4,item5,item6,link4,link5,link6}) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleDropdown = () => {
@@ -63,6 +63,22 @@ const DropdownButton2 = ({Title,item1,item2,item3,link1,link2,link3}) => {
             >
               {item3}
             </a>
+
+            <a href="#href" className='text-gray-700 block px-4 py-2 text-lg'>
+{Trans}
+            </a>
+
+            <a  href={link4} className="text-gray-700 block px-4 py-2 text-sm">
+              {item4}
+            </a>
+            <a  href={link5} className="text-gray-700 block px-4 py-2 text-sm">
+              {item5}
+            </a>
+            <a  href={link6} className="text-gray-700 block px-4 py-2 text-sm">
+              {item6}
+            </a>
+
+            
           </div>
         </div>
       )}
