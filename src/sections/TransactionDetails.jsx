f542b4e0d4792922058426291a3aa82a619f7503
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-07-20 01:45:29 +0400

    Update TransactionDetails.jsx

--- src/sections/TransactionDetails.jsx
+++ src/sections/TransactionDetails.jsx
@@ -1,71 +1,89 @@
 import React from "react";
 import "react-tooltip/dist/react-tooltip.css";
 import { Tooltip as ReactTooltip } from "react-tooltip";
+import { BsInfoCircleFill, BsCheckCircle } from "react-icons/bs";
+import { AiOutlineCopy } from "react-icons/ai";
 function TransactionDetails() {
   return (
     <section className="mt-4">
       <div className="max-w-6xl mx-auto">
         <div className="card-tran bg-white h-full rounded-xl border border-gray-45  p-[2.1rem]   mb-2">
           <div className="flex justify-between items-center mb-3">
-            <h1>Transaction Details</h1>
+            <h1 className="">Transaction Details</h1>
           </div>
-          <div className="flex flex-col  justify-between  gap-2 sm:gap-0">
-            <dl class="row">
-              <dt class="col-sm-3 col-lg-2 text-muted">
-                <span
-                  class="i-tooltip-2 "
-                  data-boundary="window"
-                  data-container="body"
-                  data-html="true"
-                  data-placement="top"
-                  data-toggle="tooltip"
-                  title="Unique character string (TxID) assigned to every verified transaction."
-                >
-                  <i class="fa-solid fa-circle-info"></i>
-                </span>
-                Transaction Hash
-              </dt>
-              <dd class="col-sm-9 col-lg-10" style={{ wordBreak: "break-all" }}>
-                <span
-                  class="transaction-details-address"
-                  data-test="transaction_detail_hash"
-                >
+          <div className="">
+            <div className="flex justify-between max-w-3xl ">
+              <div className="flex items-center gap-1">
+                <BsInfoCircleFill
+                  data-tooltip-id="my-tooltip"
+                  data-tooltip-content="Hello world!"
+                  data-tooltip-place="top"
+                  className="text-[#a3a9b5] text-sm"
+                />
+                <h2 className="text-[#6c757d] text-sm">Transactions Hash </h2>
+              </div>
+              <div className="flex">
+                <p className="flex items-center gap-1 text-sm" id="copy">
                   0x7e0778bfa42f7a96823be2ef3ca952acb51772c80cbd34f71bb65dd9463e776c{" "}
-                </span>
-                <span
-                  aria-label="Copy Transaction Hash"
-                  data-clipboard-text="0x7e0778bfa42f7a96823be2ef3ca952acb51772c80cbd34f71bb65dd9463e776c"
-                  id="app-title"
-                  class="btn-copy-icon btn-copy-icon-small btn-copy-icon-custom btn-copy-icon-no-borders"
-                  data-placement="top"
-                  data-toggle="tooltip"
-                  title="Copy Txn Hash"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 32.5 32.5"
-                    width="32"
-                    height="32"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M23.5 20.5a1 1 0 0 1-1-1v-9h-9a1 1 0 0 1 0-2h10a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1zm-3-7v10a1 1 0 0 1-1 1h-10a1 1 0 0 1-1-1v-10a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1zm-2 1h-8v8h8v-8z"
-                    ></path>
-                  </svg>
-                </span>
+                  <span>
+                    <AiOutlineCopy
+                      data-tooltip-id="copy"
+                      data-tooltip-content="Copy"
+                      data-tooltip-place="top"
+                    />
+                  </span>
+                </p>
+              </div>
+            </div>
 
-                <ReactTooltip
-        anchorId="app-title"
-        place="bottom"
-        content="copy"
-      />
-              </dd>
-            </dl>
+            <div className="flex justify-between max-w-[300px] mt-3 mb-3 ">
+              <div className="flex items-center gap-1">
+                <BsInfoCircleFill
+                  data-tooltip-id="my-tooltip"
+                  data-tooltip-content="Hello world!"
+                  data-tooltip-place="top"
+                  className="text-[#a3a9b5] text-sm"
+                />
+                <h2 className="text-[#6c757d] text-sm">Result </h2>
+              </div>
+              <div className="flex">
+                <p
+                  className="flex text-[#28a745] items-center gap-1 text-sm"
+                  id="copy"
+                >
+                  <span>
+                    <BsCheckCircle className="text-[#28a745]" />
+                  </span>
+                  Success{" "}
+                </p>
+              </div>
+            </div>
+
+            <div className="flex justify-between max-w-[545px] mt-3 mb-3 ">
+              <div className="flex items-center gap-1">
+                <BsInfoCircleFill
+                  data-tooltip-id="my-tooltip"
+                  data-tooltip-content="Hello world!"
+                  data-tooltip-place="top"
+                  className="text-[#a3a9b5] text-sm"
+                />
+                <h2 className="text-[#6c757d] text-sm">Status</h2>
+              </div>
+              <div className="flex items-center gap-3">
+                <p className=" text-[#28a745]  text-sm bg-[#28a74533] py-2 px-7 rounded-md">Confirmed </p>
+                <p className="bg-[#a3a9b596] py-2 px-7 text-sm rounded-md">Confirmed by 8,328</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
+
+      <ReactTooltip id="my-tooltip" />
+
+      <ReactTooltip id="copy" />
     </section>
   );
 }
 
 export default TransactionDetails;
+      
