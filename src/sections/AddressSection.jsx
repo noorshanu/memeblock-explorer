d455a07d2e57bf6cdfcd0b585773640770c806ae
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-07-23 02:08:23 +0400

    Update AddressSection.jsx

--- src/sections/AddressSection.jsx
+++ src/sections/AddressSection.jsx
@@ -11,7 +11,8 @@ function AddressSection() {
       <div className="max-w-6xl mx-auto">
         <div className="card-tran bg-white h-full rounded-xl border border-gray-45  p-[2.1rem]   mb-2">
           <div className="flex justify-between items-center mb-3">
-            <h1 className="">Transaction Details</h1>
+            <h1 className="">Address Details</h1>
+            <p>0x868B132291e8b93AFbD10A799Ec0Da0CE17A2e35</p>
           </div>
           <div className="">
             <div className="flex justify-between max-w-3xl ">
